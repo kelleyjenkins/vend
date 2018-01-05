502641069a0beeaceab261ddd63d9e328ac4992d
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-05 09:48:38 -0700

    test that multiple locaitons show up on snack show page-- passing

---
+++ spec/features/user_can_see_snack_page_with_info_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature "When a user visits the vending machine show" do
+  scenario "they see a list of vending machine snacks and prices" do
+    owner = Owner.create(name: "Sam's Snacks")
+    machine = owner.machines.create(location: "Don's Mixed Drinks")
+    machine2 = owner.machines.create(location: "Turing's Basement")
+
+    snack = Snack.create(name: "Twix", price: 1.50)
+
+    MachineSnack.create(snack: snack, machine: machine)
+    MachineSnack.create(snack: snack, machine: machine2)
+
+
+    visit snack_path(snack)
+
+    expect(page).to have_content(snack.name)
+    expect(page).to have_content(snack.price)
+    expect(page).to have_content(machine.location)
+    expect(page).to have_content(machine.average_price)
+    expect(page).to have_content(machine.snacks.count)
+
+    expect(page).to have_content(machine2.location)
+    expect(page).to have_content(machine2.average_price)
+    expect(page).to have_content(machine2.snacks.count)
+
+  end
+end
