c7582259964e6ad8b6ba113f05826a599159b4df
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-05 09:31:10 -0700

    adding average price test and method

--- spec/features/user_can_see_all_snacks_in_vending_machine_spec.rb
+++ spec/features/user_can_see_all_snacks_in_vending_machine_spec.rb
@@ -14,6 +14,16 @@ feature "When a user visits the vending machine show" do
     expect(page).to have_content(snack_1.price)
     expect(page).to have_content(snack_2.name)
     expect(page).to have_content(snack_2.price)
+  end
 
+  scenario "they can also see the average price of snacks in vending machine" do
+    owner = Owner.create(name: "Sam's Snacks")
+    machine = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = machine.snacks.create(name: "Twix", price: 1.50)
+    snack_2 = machine.snacks.create(name: "Hersheys", price: 1.00)
+
+    visit machine_path(machine)
+
+    expect(page).to have_content("Average Price: 1.25")
   end
 end
